d7909cf6e89d17438ca40067aa3ccef6d9ffd0a1
PikaSama <pikasama@qq.com>
2021-03-28 01:37:48 +0800

    initiation

---
+++ src/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Arcadia',
+    description: 'My wonderland.',
+    theme: 'gungnir',
+    themeConfig: {
+        personalInfo: {
+            name: "Zorin",
+            avatar: "https://cdn.jsdelivr.net/gh/PikaSama/blog-static-customs@latest/vuepress/images/avatar3.png",
+            description: "The worst pupil.",
+            sns: {
+                github: 'PikaSama',
+                email: 'pikasama@qq.com'
+            }
+        }
+    }
+}
