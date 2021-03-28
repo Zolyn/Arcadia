8046b3a2282eedea088babca0bba9e5b88ae74bb
PikaSama <pikasama@qq.com>
2021-03-28 20:54:26 +0800

    update

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: 'Arcadia',
     description: 'My wonderland.',
     theme: 'gungnir',
+    head: [
+        ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/PikaSama/blog-static-customs@2.0.1/hexo/images/icon.png' }]
+    ],
+    evergreen: true,
     themeConfig: {
         personalInfo: {
             name: "Zorin",
@@ -11,6 +15,20 @@ module.exports = {
                 github: 'PikaSama',
                 email: 'pikasama@qq.com'
             }
-        }
+        },
+        homeHeaderImages: [
+            {
+                "path":"https://cdn.jsdelivr.net/gh/PikaSama/homepage-static@2.0.0/images/bg.png"
+            }
+        ],
+        pages: {
+            tags: {
+                subtitle: '哟呼，这里是标签页~'
+            }
+        },
+        links: {
+            subtitle: '诶嘿，这里是链接页~'
+        },
+        hitokoto: true
     }
 }
