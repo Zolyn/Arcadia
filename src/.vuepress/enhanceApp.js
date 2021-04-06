5685645c1ed83ba1de42b7d47a3dd1c9f2aac0c7
PikaSama <pikasama@qq.com>
2021-04-06 21:18:56 +0800

    update

--- src/.vuepress/enhanceApp.js
+++
@@ -1,3 +0,0 @@
-import OhVueIcon from 'oh-vue-icons';
-import { CoTencentQq, RiBilibiliFill, CoGmail, RiNeteaseCloudMusicFill } from 'oh-vue-icons/icons';
-OhVueIcon.add(CoTencentQq, RiBilibiliFill, CoGmail, RiNeteaseCloudMusicFill);
