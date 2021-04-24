67ccfc96437723dd5c28b6946349cd81b2356dd7
PikaSama <pikasama@qq.com>
2021-04-24 17:16:30 +0800

    chore: config and package.json

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const { resolve } = require('path');
 
 module.exports = {
     title: 'Arcadia',
@@ -78,69 +79,7 @@ module.exports = {
             }
         },
         '@zolyn/vuepress-plugin-rightmenu': {
-            normalActions: [
-                {
-                    title: '博客源码',
-                    handler() {
-                        window.open('https://github.com/Zolyn/Arcadia');
-                    }
-                },
-                {
-                    title: '好康的',
-                    handler() {
-                        window.open('https://www.bilibili.com/video/BV1uT4y1P7CX');
-                    }
-                }
-            ],
-            eventActions: {
-                link: [
-                    {
-                        title: '在新标签页中打开链接',
-                        handler() {
-                            window.open(this.currentLink);
-                        },
-                    },
-                    {
-                        title: '复制链接',
-                        handler() {
-                            this.clipboard = this.currentLink;
-                            this.$nextTick(() => {
-                                this.copy();
-                            });
-                        },
-                    },
-                ],
-                image: [
-                    {
-                        title: '在新标签页中打开图像',
-                        handler() {
-                            window.open(this.currentImage);
-                        },
-                    },
-                    {
-                        title: '复制图像链接',
-                        handler() {
-                            this.clipboard = this.currentImage;
-                            this.$nextTick(() => {
-                                this.copy();
-                            });
-                        },
-                    },
-                ],
-            },
-            stickyActions: [
-                {
-                    title: '切换主题',
-                    handler() {
-                        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-                        if (this.$vuetify.theme.dark) {
-                            document.body.setAttribute('data-theme', 'dark');
-                        } else {
-                            document.body.setAttribute('data-theme', 'light');
-                        }
-                    },
-                },
-            ],
+            config: resolve(__dirname, 'rightmenu.js')
         }
     }
 }
---
+++ src/.vuepress/rightmenu.js
@@ -0,0 +1,65 @@
+module.exports = {
+    normalActions: [
+        {
+            title: '博客源码',
+            handler() {
+                window.open('https://github.com/Zolyn/Arcadia');
+            }
+        },
+        {
+            title: '好康的',
+            handler: function () {
+                window.open('https://www.bilibili.com/video/BV1uT4y1P7CX');
+            }
+        }
+    ],
+    eventActions: {
+        link: [
+            {
+                title: '在新标签页中打开链接',
+                handler: function () {
+                    window.open(this.currentLink);
+                },
+            },
+            {
+                title: '复制链接',
+                handler: function () {
+                    this.clipboard = this.currentLink;
+                    this.$nextTick(() => {
+                        this.copy();
+                    });
+                },
+            },
+        ],
+        image: [
+            {
+                title: '在新标签页中打开图像',
+                handler: function () {
+                    window.open(this.currentImage);
+                },
+            },
+            {
+                title: '复制图像链接',
+                handler: function () {
+                    this.clipboard = this.currentImage;
+                    this.$nextTick(() => {
+                        this.copy();
+                    });
+                },
+            },
+        ],
+    },
+    stickyActions: [
+        {
+            title: '切换主题',
+            handler: function () {
+                this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+                if (this.$vuetify.theme.dark) {
+                    document.body.setAttribute('data-theme', 'dark');
+                } else {
+                    document.body.setAttribute('data-theme', 'light');
+                }
+            },
+        },
+    ]
+}
