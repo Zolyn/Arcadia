3fcacf733ead83c53b18a19f4725bb2b261c04cd
Zorin <pikasama@qq.com>
2021-05-02 20:32:44 +0800

    test: pwa

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -127,27 +127,33 @@ module.exports = {
             tasklist: true,
         },
         '@mr-hope/pwa': {
-            favicon: '/icon.png',
+            favicon: '/icon-192.png',
             themeColor: '#377bb5',
             cachePic: true,
             maxSize: 2560,
             maxPicSize: 2048,
             manifest: {
-                lang: 'en-US',
                 icons: [
                     {
-                        src: '/icon.png',
-                        sizes: '128x128',
-                        type: 'image/png'
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                    {
+                        src: '/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
                     }
                 ]
             },
             apple: {
-                icon: '/icon.png',
-                maskIcon: '/icon.png'
+                icon: '/icon-192.png',
+                maskIcon: '/icon-192.png'
             },
             msTile: {
-                image: '/icon.png'
+                image: '/icon-192.png'
             }
         },
         'photo-swipe': {
